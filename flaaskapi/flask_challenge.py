24a6adee4eea1c35d489c1f5d92d28d879b7cd20
Dan Jahnke <maninthebox98@yahoo.com>
2021-12-01 22:14:07 +0000

    added challenge

---
+++ flaaskapi/flask_challenge.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Alta3 APIs and HTML"""
+
+## best practice says don't use commas in imports
+# use a single line for each import
+from flask import Flask
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+# This is a landing point for users (a start)
+@app.route("/") # user can land at "/"
+#@app.route("/start") # or user can land at "/start"
+def start():
+    return render_template("question.html") # look for templates/question.html
+# This is where postmaker.html POSTs data to
+# A user could also browser (GET) to this location
+
+@app.route("/answer", methods = ["POST"])
+def askquestion():
+    team_name = request.form.get("team")
+    # POST would likely come from a user interacting with postmaker.html
+    if (team_name.lower() == "green bay" or team_name.lower() == "green bay packers" or team_name.lower() == "packers"): # if the answer is correct
+            return redirect("correct")
+    else: # Ask again
+            return redirect("/")
+   
+@app.route("/correct")
+def correct():
+    return "You are correct!"
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=2224) # runs the application
+
